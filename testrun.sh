a4bb5776de42948781cf916a73ba143d5140d671
Aria Jonsdottir <ariagudlaug@gmail.com>
2017-12-14 14:24:31 +0000

    Small amount of cleanup

--- testrun.sh
+++ testrun.sh
@@ -1,6 +1,6 @@
 ant clean dist
 rm consoleoutput.txt
 rm -r testout
-hadoop-local jar dist/TweetAthleteSportMentionCount.jar TweetAthleteSportMentionCount testinput testout > consoleoutput.txt
+hadoop-local jar dist/WikiSearch.jar WikiSearch testinput testout > consoleoutput.txt
 vim consoleoutput.txt
 rm consoleoutput.txt
