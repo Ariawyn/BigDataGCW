e6ae46a44aad7e9181fa597bbf25d80e431c9e84
Aria Jonsdottir <ariagudlaug@gmail.com>
2018-01-12 15:20:59 +0000

    Cleaned up and commented inverted index mapper and reducer files

--- src/WikiSearchInvertedIndexMapper.java
+++ src/WikiSearchInvertedIndexMapper.java
@@ -12,9 +12,12 @@ public class WikiSearchInvertedIndexMapper extends Mapper<IntWritable, EnglishWi
 	// Make sure the page we are checking is actually and article
         if(page != null)
         {
-
+            // We check the page is actually an article
             if(page.isArticle())
             {
+                // This try catch block is used so that we do not have to deal with NullPointerExceptions that have been
+                // getting thrown at us from the cloud9 library. Currentl unsure of the cause, but cant fix it given its from
+                // the library function.
                 try
                 {
                     // Get the string content and title of the english wikipedia page, the content is title of the article + the actual content
@@ -37,11 +40,14 @@ public class WikiSearchInvertedIndexMapper extends Mapper<IntWritable, EnglishWi
                         boolean isValid = !word.matches("^.*[^a-zA-Z0-9 ].*$");
 
                         // We also want to check if it isnt empty
+                        // We also do not want to check any words that arent of sufficient length, this is mostly
+                        // because of server memory restrictions
                         if(word == null || word.isEmpty() || word.length() <= 3)
                         {
                             return;
                         }
 
+                        // If our word is in fact validated, we then write to the content: the word, and then the title of the article
                         if(isValid)
                         {
                             context.write(new Text(word), new Text(title));
@@ -49,7 +55,8 @@ public class WikiSearchInvertedIndexMapper extends Mapper<IntWritable, EnglishWi
                     }
                 }
                 catch(NullPointerException e)
-{
+                {
+                    // Here we catch any and all null pointer exceptions, all of them probably being thrown by the cloud9 wikipediapage getContent() library function
                     System.out.println("NullPointerException somewhere, probably with the getContent() function of the englishwikipediapage class");
                 }
             }
--- src/WikiSearchInvertedIndexReducer.java
+++ src/WikiSearchInvertedIndexReducer.java
@@ -16,25 +16,35 @@ public class WikiSearchInvertedIndexReducer extends Reducer<Text, Text, Text, Te
         // and the amount of occurances of the word as the value
         HashMap map = new HashMap();
 
-        int count = 0;
-
+        // For every article title assoiciated with the wordKey
         for(Text articleTitleText: values)
         {
+
+            // We want to get the title in a string value
             String title = articleTitleText.toString();
 
-            if(map != null && map.get(title) != null)
+            // Then check if the article title already exists
+            if(map.get(title) != null)
             {
-                count = (int)map.get(title);
+                // If it does we want to get the count of times the word has already appeared in that specified article
+                // Note: this has to be casted to an int as the hashmap type stores its value as just an Object type
+                int count = (int) map.get(title);
+                
+                // and increment it
+                count = count + 1;
 
-                map.put(title, ++count);
+                // then add it back to the map
+                map.put(title, count);
             }
             else
             {
+                // If no record of the article exists in the map for this key word, then we create one and set it to 1
                 map.put(title, 1);
             }
         }
-
+        
+        // Then we write to the context the word key, and its associated map of frequency in any articles it appeared in
+        // this essentially creates the inverted index
         context.write(wordKey, new Text(map.toString()));
-    
     }
 }
